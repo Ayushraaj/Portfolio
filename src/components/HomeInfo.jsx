b6e8188e5464e599c14961c3a9a2de8c48054007
Ayushraaj <97730432+Ayushraaj@users.noreply.github.com>
2024-07-26 21:10:37 +0530

    Update HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -6,14 +6,11 @@ const HomeInfo = ({ currentStage }) => {
   if (currentStage === 1)
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-        Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Ayush</span>
+        Hi Peeps,
+        <span className='font-semibold mx-2 text-white'>Ayush ici</span>
         👋
-        <br />
-        
-        Founder and CEO at TheCode- let's fly again.
         <br/>
-        & A boy who loves to do code...
+        who loves turning ideas into reality through code.
       </h1>
     );
 
