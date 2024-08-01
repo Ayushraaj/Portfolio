f1efb61fe128df7db261d25c623665885d09727c
Ayushraaj <97730432+Ayushraaj@users.noreply.github.com>
2024-08-01 10:52:38 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,13 +116,12 @@ export const experiences = [
         icon: q,
         iconBg: "#accbe1",
         date: "January 2023 - June 2024",
-        [
-    "Comparison Results: Quantum Annealing outperforms PCA in dimensionality reduction for predictive models, yielding lower Mean Squared Error and higher R-squared values.",
-    "Performance Insight: Quantum Annealing's superior ability to identify data patterns enhances model performance.",
-    "Optimization Potential: Fine-tuning parameters and optimizing Quantum Annealing algorithms can further improve efficiency.",
-    "Future Research: Comparing Quantum Annealing with other methods (Autoencoders, t-SNE, UMAP) and exploring scalability will enhance understanding and applications."
-],
-
+        points: [
+            "Comparison Results: Quantum Annealing outperforms PCA in dimensionality reduction for predictive models, yielding lower Mean Squared Error and higher R-squared values.",
+            "Performance Insight: Quantum Annealing's superior ability to identify data patterns enhances model performance.",
+            "Optimization Potential: Fine-tuning parameters and optimizing Quantum Annealing algorithms can further improve efficiency.",
+            "Future Research: Comparing Quantum Annealing with other methods (Autoencoders, t-SNE, UMAP) and exploring scalability will enhance understanding and applications."
+        ]
     },
     {
         title: "Research Paper 2",
@@ -131,10 +130,10 @@ export const experiences = [
         iconBg: "#fbc3bc",
         date: "Jan 2024 - July 2025",
         points: [
-            "Advancements in AI and RL: Over the past decade AI advancements particularly in reinforcement learning  have made training RL agents for custom 2D games more accessible due to improved algorithms and hardware.",
-            "A2C Model Performance: The A2C model in this study achieved the desired performance in just 1.75 million timesteps highlighting the efficiency of modern RL methods compared to traditional finite state machines in game development",
+            "Advancements in AI and RL: Over the past decade AI advancements particularly in reinforcement learning have made training RL agents for custom 2D games more accessible due to improved algorithms and hardware.",
+            "A2C Model Performance: The A2C model in this study achieved the desired performance in just 1.75 million timesteps highlighting the efficiency of modern RL methods compared to traditional finite state machines in game development.",
             "Benefits of RL in Game Development: RL agents can adapt and respond to dynamic player interactions providing a more flexible and responsive gameplay experience thus revolutionizing game development by making it more efficient and adaptive.",
-            "Future Research Directions: Future work could focus on hyperparameter tuning extended training runs and developing custom algorithms to optimize ray perception sensor distances for vehicle navigation.",
+            "Future Research Directions: Future work could focus on hyperparameter tuning, extended training runs, and developing custom algorithms to optimize ray perception sensor distances for vehicle navigation.",
         ],
     },
     {
@@ -163,7 +162,6 @@ export const experiences = [
 ];
 
 export const socialLinks = [
-    
     {
         name: 'GitHub',
         iconUrl: github,
